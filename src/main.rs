349b6fdf276e2edd952ec83815c5cc13a8b3a936
Innealtoir <innealtoir.codecim@protonmail.com>
2017-08-20 16:58:34 +0000

    updating

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 # listens for tcpipv6
+pub sturct AddrParseError(_);
+use std::net::{TcpListener, TcpStream};
 
-
-
-fn main() {
-
+fn handle_client(stream: TcpStream) {
+	// ...
 }
+
+let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+
+for stream in listener.incoming() {
+	handle_client(stream?};
+}
+
